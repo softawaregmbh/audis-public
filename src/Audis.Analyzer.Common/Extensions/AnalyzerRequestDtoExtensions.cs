554a7c4580f7153aed3d3a96d3c0e673c300c1cf
Michael Einsiedler <michael.einsiedler@softaware.at>
2019-10-10 08:39:20 +0200

    add extension methods for Audis.Dto and Audis.Analyzer.Common

---
+++ src/Audis.Analyzer.Common/Extensions/AnalyzerRequestDtoExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Audis.Primitives;
+
+namespace Audis.Analyzer.Common.Extensions
+{
+    public static class AnalyzerRequestDtoExtensions
+    {
+        public static bool TryGetSingleKnowledgeValue(this AnalyzerRequestDto analyzerRequestDto, KnowledgeIdentifier knowledgeIdentifier, out KnowledgeValue value)
+        {
+            var knowledge = analyzerRequestDto.Knowledge.FirstOrDefault(k => k.KnowledgeIdentifier == knowledgeIdentifier);
+
+            if (knowledge == null)
+            {
+                value = null;
+                return false;
+            }
+
+            var knowledgeValue = knowledge.Values.SingleOrDefault()?.KnowledgeValue;
+
+            if (knowledgeValue == null)
+            {
+                value = null;
+                return false;
+            }
+
+            value = knowledgeValue;
+            return true;
+        }
+
+        public static bool IsAlreadyHandled(this AnalyzerRequestDto analyzerRequestDto, string key, string value)
+        {
+            return analyzerRequestDto.Data != null && (value == analyzerRequestDto.Data.Value<string>(key));
+        }
+    }
+}
---
+++ src/Audis.Dtos/Extensions/KnowledgeDtoExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Audis.Dtos.Extensions
+{
+    public static class KnowledgeDtoExtensions
+    {
+        public static SuggestedKnowledgeDto From(string knowledgeIdentifier, string value)
+        {
+            return new SuggestedKnowledgeDto()
+            {
+                KnowledgeIdentifier = KnowledgeIdentifier.From(knowledgeIdentifier),
+                Values = new HashSet<KnowledgeValueDto>()
+                {
+                    new KnowledgeValueDto()
+                    {
+                        KnowledgeValue = KnowledgeValue.From(value)
+                    }
+                }
+            };
+        }
+    }
+}
