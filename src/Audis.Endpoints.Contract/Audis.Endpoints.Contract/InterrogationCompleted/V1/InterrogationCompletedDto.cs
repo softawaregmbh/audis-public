ef63b2274f0820141e80a3ec0a9c395e967118e9
Daniel Sklenitzka <daniel.sklenitzka@softaware.at>
2020-02-06 16:55:26 +0100

    Modified DTOs, changed version to beta3

--- src/Audis.Endpoints.Contract/Audis.Endpoints.Contract/InterrogationCompleted/V1/InterrogationCompletedDto.cs
+++ src/Audis.Endpoints.Contract/Audis.Endpoints.Contract/InterrogationCompleted/V1/InterrogationCompletedDto.cs
@@ -6,9 +6,20 @@ namespace Audis.Endpoints.Contract.InterrogationCompleted.V1
 {
     public class InterrogationCompletedDto
     {
-        public Guid Id { get; set; }
+        public Guid InterrogationId { get; set; }
         public int FinalProcessStepId { get; set; }
+        public DateTime Timestamp { get; set; }
+        public TenantId TenantId { get; set; }
+        public RevisionId RevisionId { get; set; }
+        public string ExternalId { get; set; }
+        public string UserId { get; set; }
         public string FreeText { get; set; }
-        public IReadOnlyCollection<ScenarioIdentifier> SelectedScenarios { get; set; }
+        public bool Cancelled { get; set; }
+        public string CancellationReason { get; set; }
+        public ScenarioIdentifier SuggestedScenarioIdentifier { get; set; }
+        public string SuggestedScenarioName { get; set; }
+        public ScenarioIdentifier SelectedScenarioIdentifier { get; set; }
+        public string SelectedScenarioName { get; set; }
+        public string SelectedScenarioReason { get; set; }
     }
 }
---
+++ src/Audis.Endpoints.Contract/Audis.Endpoints.Contract/InterrogationUpdated/V1/InterrogationStepDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Audis.Primitives;
+using Newtonsoft.Json.Linq;
+
+namespace Audis.Endpoints.Contract.InterrogationUpdated.V1
+{
+    public class InterrogationStepDto
+    {
+        public Guid InterrogationId { get; set; }
+        public int ProcessStepId { get; set; }
+        public DateTime Timestamp { get; set; }
+        public TenantId TenantId { get; set; }
+        public RevisionId RevisionId { get; set; }
+        public string ExternalId { get; set; }
+        public string UserId { get; set; }
+        public QuestionId CurrentQuestionId { get; set; }
+        public string CurrentQuestion { get; set; }
+        public string CurrentQuestionType { get; set; }
+        public IReadOnlyCollection<KnowledgeDto> Knowledge { get; set; }
+        public ScenarioIdentifier SuggestedScenarioIdentifier { get; set; }
+        public string SuggestedScenarioName { get; set; }
+
+        /// <summary>
+        /// Free data object where implementation-specific data/identification/... can be stored.
+        /// </summary>
+        public JObject Data { get; set; }
+    }
+}
