c90ccddee25e0b70c770ee733c6151e2d81a2d87
Daniel Sklenitzka <daniel.sklenitzka@softaware.at>
2020-03-17 09:50:14 +0100

    Added CommentAddedDto, increased version

---
+++ src/Audis.Endpoints.Contract/Audis.Endpoints.Contract/CommentAdded/V1/CommentAddedDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Audis.Primitives;
+
+namespace Audis.Endpoints.Contract.CommentAdded.V1
+{
+    public class CommentAddedDto
+    {
+        public Guid CommentId { get; set; }
+        public string Comment { get; set; } = default!;
+        public Guid InterrogationId { get; set; }
+        public DateTime Timestamp { get; set; }
+        public TenantId TenantId { get; set; } = default!;
+        public RevisionId RevisionId { get; set; } = default!;
+        public string? ExternalId { get; set; }
+        public string UserId { get; set; } = default!;
+    }
+}
