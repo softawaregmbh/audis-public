5132bcb161d2e412f2b223d85ecd68e72c368e0e
Michael Einsiedler <michael.einsiedler@softaware.at>
2020-04-02 14:38:22 +0200

    add knowledge enricher project

---
+++ src/Audis.KnowledgeEnricher.Contract/Audis.KnowledgeEnricher.Contract/V1/IKnowledgeEnricher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Audis.KnowledgeEnricher.Contract.V1
+{
+    /// <summary>
+    /// Takes some knowledge and adds further knowledge based on existing knowledge.
+    /// </summary>
+    public interface IKnowledgeEnricher
+    {
+        /// <summary>
+        /// Takes some knowledge and adds further knowledge based on the existing knowledge. Concrete implementations can inherit specific knowledge based on input knowledge.
+        /// </summary>
+        /// <param name="knowledge">The knowledge used as starting point for inheriting knowledge.</param>
+        /// <returns>Returns the given knowledge plus potentially more inherited knowledge.</returns>
+        Task<IReadOnlyCollection<KnowledgeDto>> EnrichKnowledge(Task<IReadOnlyCollection<KnowledgeDto>> knowledge);
+    }
+}
