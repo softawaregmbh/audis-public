a89c3cda109a6afefe70e88ec3dc65ca792fb10d
christophzeltner <christoph.zeltner@softaware.at>
2020-05-25 11:47:40 +0200

    AUDIS-3 implementing feedback of https://github.com/softawaregmbh/audis-public/pull/7 changed version to .netstandard 2.1 to enable nullable flag in project added versions to projectfile removed enum and replaced it with a string in referencing components

--- src/Audis.Location/Audis.Location/AutocompleteLocation/V1/AutocompleteLocationDto.cs
+++ src/Audis.Location/Audis.Location/AutocompleteLocation/V1/AutocompleteLocationDto.cs
@@ -1,20 +1,17 @@
-﻿using Audis.Location.Enums.V1;
-
-namespace Audis.Location.AutocompleteLocation.V1
+﻿namespace Audis.Location.AutocompleteLocation.V1
 {
     public class AutocompleteLocationDto
     {
-        public AutocompleteLocationDto(string location, string placeId, LocationProviderType locationProviderType)
+        public AutocompleteLocationDto(string location, string placeId, string locationProvider)
         {
             this.Location = location;
             this.PlaceId = placeId;
-            this.LocationProviderType = locationProviderType;
+            this.LocationProvider = locationProvider;
         }
 
         public string Location { get; set; }
         public string PlaceId { get; set; }
         public string LocationProvider { get; set; }
-        public LocationProviderType LocationProviderType { get; set; }
     }
 }
   
--- src/Audis.Location/Audis.Location/LocationAddress/V1/LocationAddressDto.cs
+++ src/Audis.Location/Audis.Location/LocationAddress/V1/LocationAddressDto.cs
@@ -5,9 +5,9 @@
         public double Latitude { get; set; }
         public double Longitude { get; set; }
 
-        public string Street { get; set; }
-        public string StreetNumber { get; set; }
-        public string City { get; set; }
-        public string ZipCode { get; set; }
+        public string? Street { get; set; }
+        public string? StreetNumber { get; set; }
+        public string? City { get; set; }
+        public string? ZipCode { get; set; }
     }
 }
