9daaf2cc48cb787a363d82421541ab20a4e335c7
christophzeltner <christoph.zeltner@softaware.at>
2020-05-25 14:02:10 +0200

    set netstandard version to 2.0 and use latest c# version long and latitude can be nullable

--- src/Audis.Location/Audis.Location/LocationAddress/V1/LocationAddressDto.cs
+++ src/Audis.Location/Audis.Location/LocationAddress/V1/LocationAddressDto.cs
@@ -2,8 +2,8 @@
 {
     public class LocationAddressDto
     {
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
 
         public string? Street { get; set; }
         public string? StreetNumber { get; set; }
--- src/Audis.Location/Audis.Location/LocationAddress/V1/LocationAddressRequestDto.cs
+++ src/Audis.Location/Audis.Location/LocationAddress/V1/LocationAddressRequestDto.cs
@@ -6,6 +6,7 @@ namespace Audis.Location.LocationAddress.V1
 {
     public class LocationAddressRequestDto
     {
+        // either address, placeid or both can be set in the analyser
         public string? Address { get; set; }
         public string? PlaceId { get; set; }
         public string LocationProvider { get; set; }
