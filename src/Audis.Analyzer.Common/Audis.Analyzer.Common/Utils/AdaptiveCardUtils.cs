853a8ee40f39a83cc7c6258a5b1c9ca78f785bc6
Manuel Fuchs <manuel.fuchs@softaware.at>
2020-07-13 14:41:42 +0200

    add adaptive card utils

---
+++ src/Audis.Analyzer.Common/Audis.Analyzer.Common/Utils/AdaptiveCardUtils.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using AdaptiveCards.Templating;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Audis.Analyzer.Common.Utils
+{
+    public class AdaptiveCardUtils
+    {
+        public static async Task<string> ConstructAsync(IReadOnlyCollection<string> templatePaths)
+        {
+            var baseCardString = @"
+{{
+  ""type"": ""AdaptiveCard"",
+  ""$schema"": ""http://adaptivecards.io/schemas/adaptive-card.json"",
+  ""version"": ""1.2""
+}}";
+
+            var adaptiveCard = JObject.Parse(baseCardString);
+
+            var body = new JArray();
+            foreach (var templatePath in templatePaths)
+            {
+                using (var templateStream = new StreamReader(File.OpenRead(templatePath)))
+                {
+                    body.Merge(JArray.Parse(await templateStream.ReadToEndAsync()));
+                }
+            }
+
+            adaptiveCard["body"] = body;
+
+            return JsonConvert.SerializeObject(adaptiveCard);
+        }
+
+        public static string ExpandTemplate(string cardTemplateString, JObject evaluationContextRoot)
+        {
+            var cardTemplate = new AdaptiveCardTemplate(cardTemplateString);
+            return cardTemplate.Expand(new EvaluationContext(evaluationContextRoot));
+        }
+    }
+}
