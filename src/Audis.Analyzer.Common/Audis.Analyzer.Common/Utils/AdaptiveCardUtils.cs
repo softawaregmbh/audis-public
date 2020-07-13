4fa8d3c07303ca60fde0c2a508be4ec6c7dfba2a
Manuel Fuchs <manuel.fuchs@softaware.at>
2020-07-13 17:31:37 +0200

    improve AdaptiveCard utility method

--- src/Audis.Analyzer.Common/Audis.Analyzer.Common/Utils/AdaptiveCardUtils.cs
+++ src/Audis.Analyzer.Common/Audis.Analyzer.Common/Utils/AdaptiveCardUtils.cs
@@ -9,35 +9,32 @@ namespace Audis.Analyzer.Common.Utils
 {
     public class AdaptiveCardUtils
     {
-        public static async Task<string> ConstructAsync(IReadOnlyCollection<string> templatePaths)
+        public static async Task<string> ConstructAsync(IEnumerable<(string, JObject)> cardTemplates)
         {
+            var body = new JArray();
+            foreach (var (templatePath, evaluationContext) in cardTemplates)
+            {
+                string cardString;
+                using (var templateStream = new StreamReader(File.OpenRead(templatePath)))
+                {
+                    var templateString = await templateStream.ReadToEndAsync();
+                    cardString = new AdaptiveCardTemplate(templateString)
+                        .Expand(evaluationContext);
+                }
+
+                body.Merge(JArray.Parse(cardString));
+            }
+
             var baseCardString = @"
-{{
+{
   ""type"": ""AdaptiveCard"",
   ""$schema"": ""http://adaptivecards.io/schemas/adaptive-card.json"",
   ""version"": ""1.2""
-}}";
+}";
 
             var adaptiveCard = JObject.Parse(baseCardString);
-
-            var body = new JArray();
-            foreach (var templatePath in templatePaths)
-            {
-                using (var templateStream = new StreamReader(File.OpenRead(templatePath)))
-                {
-                    body.Merge(JArray.Parse(await templateStream.ReadToEndAsync()));
-                }
-            }
-
             adaptiveCard["body"] = body;
-
             return JsonConvert.SerializeObject(adaptiveCard);
-        }
-
-        public static string ExpandTemplate(string cardTemplateString, JObject evaluationContextRoot)
-        {
-            var cardTemplate = new AdaptiveCardTemplate(cardTemplateString);
-            return cardTemplate.Expand(new EvaluationContext(evaluationContextRoot));
         }
     }
 }
