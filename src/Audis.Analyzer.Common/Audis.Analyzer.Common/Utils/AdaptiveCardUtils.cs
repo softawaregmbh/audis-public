1d79312e228b083b70dd69b70babc82fd133c852
Manuel Fuchs <manuel.fuchs@softaware.at>
2020-07-14 08:56:55 +0200

    document AdaptiveCardUtils.ConstructAsync; name ValueTuple elements; alter file access method; change JObject to a simple object instance

--- src/Audis.Analyzer.Common/Audis.Analyzer.Common/Utils/AdaptiveCardUtils.cs
+++ src/Audis.Analyzer.Common/Audis.Analyzer.Common/Utils/AdaptiveCardUtils.cs
@@ -9,13 +9,25 @@ namespace Audis.Analyzer.Common.Utils
 {
     public class AdaptiveCardUtils
     {
-        public static async Task<string> ConstructAsync(IEnumerable<(string, JObject)> cardTemplates)
+        /// <summary>
+        /// Constructs a new adaptive card based on at least one adaptive card template.
+        /// </summary>
+        /// <param name="cardTemplates">
+        /// A <see cref="IEnumerable{T}"/> containing at least one <see cref="System.ValueTuple"/> bag,
+        /// symbolizing one adaptive card instance used to construct the new adaptive card.
+        /// The first bag-element is the path to the adaptive card template, the second element is
+        /// the data object that should be applied on this template.
+        /// </param>
+        /// <returns>
+        /// A <see cref="Task{TResult}"/> returning the constructed adaptive card as serialized JSON string.
+        /// </returns>
+        public static async Task<string> ConstructAsync(IEnumerable<(string templatePath, object data)> cardTemplates)
         {
             var body = new JArray();
             foreach (var (templatePath, evaluationContext) in cardTemplates)
             {
                 string cardString;
-                using (var templateStream = new StreamReader(File.OpenRead(templatePath)))
+                using (var templateStream = File.OpenText(templatePath))
                 {
                     var templateString = await templateStream.ReadToEndAsync();
                     cardString = new AdaptiveCardTemplate(templateString)
