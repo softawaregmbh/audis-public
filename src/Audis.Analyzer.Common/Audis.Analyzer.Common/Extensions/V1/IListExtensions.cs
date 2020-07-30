5a4ce22bb03b83f7228d9ddaed243f2f016c3b36
Manuel Fuchs <manuel.fuchs@softaware.at>
2020-07-30 11:16:35 +0200

    add list extensions

---
+++ src/Audis.Analyzer.Common/Audis.Analyzer.Common/Extensions/V1/IListExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Audis.Analyzer.Contract.V1;
+using Audis.Primitives;
+
+namespace Audis.Analyzer.Common.Extensions.V1
+{
+    public static class IListExtensions
+    {
+        public static void Add(
+            this IList<SuggestedKnowledgeDto> suggestedKnowledges,
+            string identifier,
+            string value,
+            double probability = 1.0,
+            DateTime? lastUpdated = null,
+            KnowledgeOrigin origin = KnowledgeOrigin.Client)
+        {
+            if (value != null)
+            {
+                suggestedKnowledges.Add(new SuggestedKnowledgeDto
+                {
+                    KnowledgeIdentifier = KnowledgeIdentifier.From(identifier),
+                    Values = new HashSet<KnowledgeValueDto>(new KnowledgeValueDto[]
+                    {
+                        new KnowledgeValueDto
+                        {
+                            KnowledgeValue = KnowledgeValue.From(value)
+                        }
+                    }),
+                    Probability = probability,
+                    LastUpdated = lastUpdated ?? DateTime.UtcNow,
+                    Origin = origin
+                });
+            }
+        }
+    }
+}
