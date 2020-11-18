d4e6f39593d4048a378ea33b838622d857f42c96
Michael Einsiedler <meinsiedler@users.noreply.github.com>
2020-11-18 14:52:28 +0100

    Primitive Records (#15)
    * convert Audis.Primitives to records
    
    * make typeconverter only available for strings, add unit tests
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    Use .NET 5
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    Use .NET 5
    
    * add test-task for Audis.Primitives to pipelines
    
    * publish test results as TRX
    
    * use TRX logger for dotnet test

---
+++ src/Audis.Primitives/Audis.Primitives/Primitive.cs
@@ -0,0 +1,15 @@
+﻿namespace Audis.Primitives
+{
+    /// <summary>
+    /// Base record for wrapping primitive types.
+    /// Used to prevent primitive obsession, see https://refactoring.guru/smells/primitive-obsession.
+    /// </summary>
+    /// <typeparam name="TValue">The type of the wrapped value.</typeparam>
+    public record Primitive<TValue>(TValue Value)
+    {
+        public override string? ToString()
+        {
+            return this.Value?.ToString();
+        }
+    }
+}
