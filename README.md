c38d2487f0ef8aad159530e17d888b6ce18da1e4
Michael Einsiedler <michael.einsiedler@softaware.at>
2020-11-23 12:38:28 +0100

    KnowledgeEnrichers: add flag if enriching happens within start of interrogation

--- README.md
+++ README.md
@@ -6,7 +6,8 @@ The repository contains the following packages, also available on [NuGet](https:
 | Package | Description |
 | --- | --- |
 [`Audis.Primitives`](src/Audis.Primitives) | Provides primitive data types used in Audis, e.g. KnowlegeIdentifiers, KnowledgeValues, DispositionLevels, ScenarioIdentifier, ... |
-[`Audis.Dtos`](src/Audis.Dtos) | Provides commonly used DTOs for interrogations, knowledges, scenarios, ... |
 [`Audis.Analyzer.Common`](src/Audis.Analyzer.Common) | Provides DTOs and interfaces for implementing analyzers. |
+[`Audis.Analyzer.Contract`](src/Audis.Analyzer.Contract) | Provides common DTOs and extension methods for Analyzers. |
 [`Audis.Endpoints.Contract`](src/Audis.Endpoints.Contract) | Defines DTOs for endpoints which are called or consumed by Audis. |
+[`Audis.KnowledgeEnrichers.Contract`](src/Audis.KnowledgeEnrichers.Contract) | Provides an interface and DTOs for knowledge enrichers. |
 [`Audis.Location`](src/Audis.Location) | Defines DTOs for the location endpoint called by Audis. |
--- src/Audis.KnowledgeEnrichers.Contract/Audis.KnowledgeEnrichers.Contract/V1/IKnowledgeEnricher.cs
+++ src/Audis.KnowledgeEnrichers.Contract/Audis.KnowledgeEnrichers.Contract/V1/IKnowledgeEnricher.cs
@@ -13,8 +13,12 @@ namespace Audis.KnowledgeEnrichers.Contract.V1
         /// Takes some knowledge and returns only the inherited knowlege if the knowledge enricher has anything to add for the given knowledge.
         /// </summary>
         /// <param name="knowledge">The knowledge used as starting point for inheriting knowledge.</param>
+        /// <param name="isStartOfInterrogation"><see langword="True"/> if the enricher is called when starting the interrogation.</param>
         /// <param name="configuration">Optional configuration for the knowledge enricher.</param>
         /// <returns>Returns the inherited knowledge or an empty list.</returns>
-        Task<IReadOnlyCollection<KnowledgeDto>> EnrichKnowledgeAsync(IReadOnlyCollection<KnowledgeDto> knowledge, JObject? configuration = null);
+        Task<IReadOnlyCollection<KnowledgeDto>> EnrichKnowledgeAsync(
+            IReadOnlyCollection<KnowledgeDto> knowledge,
+            bool isStartOfInterrogation,
+            JObject? configuration = null);
     }
 }
