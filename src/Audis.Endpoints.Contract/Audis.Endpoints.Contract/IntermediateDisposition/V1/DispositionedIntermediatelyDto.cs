a9628b6fb56ce09c6dd95632df32d1b1fc3bad02
Alexander Stelzhammer <alexander.stelzhammer@softaware.at>
2020-12-04 12:22:54 +0100

    feat: DispositionedIntermediatelyDto

---
+++ src/Audis.Endpoints.Contract/Audis.Endpoints.Contract/IntermediateDisposition/V1/DispositionedIntermediatelyDto.cs
@@ -0,0 +1,30 @@
+﻿using Audis.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Audis.Endpoints.Contract.IntermediateDisposition.V1
+{
+    public class DispositionedIntermediatelyDto
+    {
+        public Guid InterrogationId { get; set; }
+
+        public int CurrentProcessStepId { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        public TenantId TenantId { get; set; } = default!;
+
+        public RevisionId RevisionId { get; set; } = default!;
+
+        public string? ExternalId { get; set; }
+
+        public string? UserId { get; set; }
+
+        public string KnowledgeSummary { get; set; } = default!;
+
+        public string? CurrentScenarioIdentifier { get; set; }
+        
+        public string? CurrentScenarioName { get; set; }
+    }
+}
