7dc2e9bd91ef79bf9b96a3f3039f87b593acf940
Christoph Gattermayr <christoph.gattermayr@proton.me>
2026-03-04 11:50:53 +0100

    feat: #230 extend endpoints and enricher contracts with knowledge metadata

---
+++ src/Audis.Endpoints.Contract/Audis.Endpoints.Contract/KnowledgeSummaryGenerated/V1/KnowledgeSummaryGeneratedDto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using Audis.Endpoints.Contract.Shared.V1;
+using Audis.Primitives;
+
+namespace Audis.Endpoints.Contract.KnowledgeSummaryGenerated.V1;
+
+public class KnowledgeSummaryGeneratedDto
+{
+    public Guid InterrogationId { get; set; }
+    
+    public DateTime Timestamp { get; set; }
+
+    required public TenantId TenantId { get; set; }
+
+    required public RevisionId RevisionId { get; set; }
+
+    public string? ExternalId { get; set; }
+
+    public string? Logon { get; set; }
+
+    public string? UserName { get; set; }
+
+    public string? KnowledgeSummary { get; set; }
+
+    public IReadOnlyCollection<KnowledgeSummaryDto>? KnowledgeSummaryItems { get; set; }
+    
+    public IReadOnlyCollection<KnowledgeDto> Knowledge { get; set; } = new List<KnowledgeDto>();
+
+    public ScenarioDto? SuggestedScenario { get; set; }
+
+    /// <summary>
+    ///     Free data object where implementation-specific data/identification/... can be stored.
+    /// </summary>
+    public JsonElement? Data { get; set; }
+}
