543b08eeae33a4e6df86bc6336c01df512326e8a
Christoph Gattermayr <christoph.gattermayr@softaware.at>
2026-04-16 11:22:42 +0200

    chore: formatting

--- src/Audis.Primitives/Audis.Contract/V1/KnowledgeMetadata.cs
+++ src/Audis.Primitives/Audis.Contract/V1/KnowledgeMetadata.cs
@@ -11,20 +11,23 @@ public class KnowledgeMetadata
         this.Flags = flags;
     }
 
+    public int Priority { get; init; }
+
     public KnowledgeFlags Flags { get; init; }
 
     public bool IsUnknownAnswerKnowledge => this.Flags.HasFlag(KnowledgeFlags.UnknownAnswerKnowledge);
 
     public bool IsKnowledgeSummaryAnswerKnowledge => this.Flags.HasFlag(KnowledgeFlags.KnowledgeSummaryAnswerKnowledge);
+
     public bool ShouldIgnoreInTimeLine => this.Flags.HasFlag(KnowledgeFlags.IgnoreInTimeLine);
+
     public bool ShouldIgnoreInSummary => this.Flags.HasFlag(KnowledgeFlags.IgnoreInSummary);
+
     public bool IsInputAnswerKnowledge => this.Flags.HasFlag(KnowledgeFlags.InputAnswerKnowledge);
 
     public bool IsStandaloneKnowledge => this.Flags.HasFlag(KnowledgeFlags.StandaloneKnowledge);
 
     public bool IsProtectedFromVolatileSource => this.Flags.HasFlag(KnowledgeFlags.ProtectedFromVolatileSource);
-
-    public int Priority { get; init; }
 
     public KnowledgeMetadata Clone() =>
         new()
