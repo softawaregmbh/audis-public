d5e313b702951a5411135b2e74ef245422bf1cb5
Christoph Gattermayr <christoph.gattermayr@softaware.at>
2026-06-25 08:03:10 +0200

    feat: endpoints.contract 3.3.1 (add knowledge in CommentAddedDto v2)

---
+++ src/Audis.Endpoints.Contract/Audis.Endpoints.Contract/CommentAdded/V2/CommentAddedDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Audis.Endpoints.Contract.Shared.V1;
+using Audis.Primitives;
+
+namespace Audis.Endpoints.Contract.CommentAdded.V2;
+
+public class CommentAddedDto
+{
+    public Guid CommentId { get; set; }
+    public string Comment { get; set; } = default!;
+    public Guid InterrogationId { get; set; }
+    public DateTime Timestamp { get; set; }
+    public TenantId TenantId { get; set; } = default!;
+    public RevisionId RevisionId { get; set; } = default!;
+    public string? ExternalId { get; set; }
+    public string? UserId { get; set; }
+    public IReadOnlyCollection<KnowledgeDto> Knowledge { get; set; } = new List<KnowledgeDto>();
+}
